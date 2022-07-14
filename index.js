1e121f4e64202ee755bd9e40d00b55eb9dcb7948
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-07-14 20:30:57 +0200

    was relatively simpler

--- index.js
+++ index.js
@@ -2,18 +2,30 @@
 
 // Otherwise returns false:
 
-
 function checkAge(age) {
-
+  if (age > 18) {
+    return true;
+  } else {
+    return false;
   }
+}
 
+console.log(checkAge(17));
 
 // Write a function min(a,b) which returns the least of two numbers a and b.
-  function min(a, b) {
-   
+function min(a, b) {
+  if (a > b) {
+    return b;
+  } else {
+    return a;
   }
+}
+console.log(min(-87687681, 78));
 
 // Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
-  function pow(x, n) {
-    
-  }
+
+function pow(x, n) {
+  return x ** n;
+}
+
+console.log(pow(3, 3));
